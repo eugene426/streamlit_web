cd96fd1abe4b898d73e74248e1ab93054bb9124a
eugene <eugenekim1016@gmail.com>
2024-06-05 22:05:53 +0900

    Create python streamlit project

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+import pandas as np
+
+from time import sleep
+
+st.set_page_config(
+    page_icon='🍑',
+    page_title = '김유진 페이지 테스트',
+    layout='wide'
+)
+
+st.header('김유진 페이지에 오신 걸 환영해요!')
+st.subheader('키키키')
+
